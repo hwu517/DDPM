926316ae440bb336aa56d81fdc156856cbe9c892
hwu517 <heranwu653@gmail.com>
2024-10-23 19:13:34 +1300

    classifier.py

---
+++ classifier.py
@@ -0,0 +1,12 @@
+import tensorflow as tf
+from tensorflow.keras import Sequential
+from tensorflow.keras.layers import Dense, Dropout
+
+def create_classifier(input_shape, num_classes):
+    classifier = Sequential([
+        Dense(128, activation='relu', input_shape=input_shape),
+        Dropout(0.5),
+        Dense(num_classes, activation='softmax')
+    ])
+    classifier.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+    return classifier
